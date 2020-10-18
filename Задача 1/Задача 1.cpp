ff60a6d9e9b68f41c6c9f8843ee009b5f5a8741b
Бабиков Дмитрий <dima.babikov.02@mail.ru>
2020-10-18 14:54:19 +0300

    Добавьте файлы проекта.

---
+++ Задача 1/Задача 1.cpp
@@ -0,0 +1,20 @@
+﻿#include <iostream>
+using namespace std;
+
+void output(const int a)
+{
+    for (int i = 1; i <= a; i++)
+    {
+        if (!(a % i)) cout << i << "  ";
+    }
+}
+
+int main()
+{
+    setlocale(LC_ALL, "rus");
+    int a;
+    cout << "Введите число: \n";
+    cin >> a;
+    output(a);
+    return 0;
+}
